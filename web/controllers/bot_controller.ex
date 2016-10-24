1c83b275249fb60ab24e76fcd0b7650a1a44ab62
Bruno Azenha Gonçalves <azenha.bruno@gmail.com>
2016-10-24 16:38:13 -0200

    Adds lot's of cats with the cat API
    It is a little confused with buttons for Cats and Dogs, but I'm pretty happy with this, as it was exactly as completed at the Elixir meeting.

--- web/controllers/bot_controller.ex
+++ web/controllers/bot_controller.ex
@@ -14,6 +14,7 @@ defmodule KittyBot.BotController do
     case postback do
       "CMD_GET_STARTED" -> FacebookMessenger.Sender.send_template(sender, create_carroussel)
       "CMD_HELP" -> FacebookMessenger.Sender.send(sender, "AJUDAAAAA")
+      "CMD_I_WANT_CATS" -> FacebookMessenger.Sender.send_template(sender, create_cat_carroussel)
       _ -> {:ok}
     end
   end
@@ -22,18 +23,38 @@ defmodule KittyBot.BotController do
     carroussel = [
       %{title: "Puppy",
       image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
-      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      buttons: [ create_payload_button("More Cats!", "CMD_I_WANT_CATS") ]
       },
       %{title: "Puppy",
       image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
-      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      buttons: [ create_payload_button("More Cats!", "CMD_I_WANT_CATS") ]
       },
       %{title: "Puppy",
       image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
-      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      buttons: [ create_payload_button("More Cats!", "CMD_I_WANT_CATS") ]
       },
       %{title: "Puppy",
       image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
+      buttons: [ create_payload_button("More Cats!", "CMD_I_WANT_CATS") ]
+      }]
+  end
+
+  defp create_cat_carroussel do
+    carroussel = [
+      %{title: "Cat",
+      image_url: gimme_a_cat_link,
+      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      },
+      %{title: "Cat",
+      image_url: gimme_a_cat_link,
+      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      },
+      %{title: "Cat",
+      image_url: gimme_a_cat_link,
+      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      },
+      %{title: "Cat",
+      image_url: gimme_a_cat_link,
       buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
       }]
   end
@@ -44,6 +65,12 @@ defmodule KittyBot.BotController do
       title: title,
       payload: payload
     }
+  end
+
+  def gimme_a_cat_link() do
+    response = HTTPotion.get "http://thecatapi.com/api/images/get?format=html&size=med&type=jpg"
+    body = Map.fetch!(response, :body)
+    Regex.run(~r/img src="(http.*jpg)/, body, capture: :all_but_first) |> hd
   end
 
 end

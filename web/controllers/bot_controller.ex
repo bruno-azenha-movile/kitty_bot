0c0f5fcc5734e4da348d5ef3a1139ee71e895dad
Bruno Azenha Gonçalves <azenha.bruno@gmail.com>
2016-10-24 14:36:34 -0200

    Add puppy carrousel

--- web/controllers/bot_controller.ex
+++ web/controllers/bot_controller.ex
@@ -12,8 +12,38 @@ defmodule KittyBot.BotController do
 
   defp handle_postback(sender, postback) do
     case postback do
-      "CMD_GET_STARTED" -> FacebookMessenger.Sender.send(sender, "Você Apertou o GET STARTED")
+      "CMD_GET_STARTED" -> FacebookMessenger.Sender.send_template(sender, create_carroussel)
+      "CMD_HELP" -> FacebookMessenger.Sender.send(sender, "AJUDAAAAA")
       _ -> {:ok}
     end
   end
+
+  defp create_carroussel() do
+    carroussel = [
+      %{title: "Puppy",
+      image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
+      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      },
+      %{title: "Puppy",
+      image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
+      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      },
+      %{title: "Puppy",
+      image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
+      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      },
+      %{title: "Puppy",
+      image_url: "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-30.jpg",
+      buttons: [ create_payload_button("More Cats!", "CMD_GET_STARTED") ]
+      }]
+  end
+
+  defp create_payload_button(title, payload) do
+    %{
+      type: "postback",
+      title: title,
+      payload: payload
+    }
+  end
+
 end

764599f0c17c556bad17502460dac654798f2d3b
Bruno Azenha Gonçalves <azenha.bruno@gmail.com>
2016-10-24 14:24:55 -0200

    Add simple GET STARTED MESSAGE

--- web/controllers/bot_controller.ex
+++ web/controllers/bot_controller.ex
@@ -4,6 +4,16 @@ defmodule KittyBot.BotController do
   def message_received(msg) do
     text = FacebookMessenger.Response.message_texts(msg) |> hd
     sender = FacebookMessenger.Response.message_senders(msg) |> hd
+    postback = FacebookMessenger.Response.message_postback(msg) |> hd
+
+    handle_postback(sender, postback)
     FacebookMessenger.Sender.send(sender, text)
+  end
+
+  defp handle_postback(sender, postback) do
+    case postback do
+      "CMD_GET_STARTED" -> FacebookMessenger.Sender.send(sender, "Você Apertou o GET STARTED")
+      _ -> {:ok}
+    end
   end
 end
